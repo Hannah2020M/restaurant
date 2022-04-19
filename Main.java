dde56a21dc4c7571cbc41220e24a435670ab734e
Hannah2020M <coder2020aug@gmail.com>
2022-04-19 00:59:34 -0500

    added colesalw

--- Main.java
+++ Main.java
@@ -1,15 +1,14 @@
 package restaurant;
-import restaurant.MenuItems;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Scanner;
+
 
 public class Main {
 
     public static void main(String[] args) {
         MenuItems item1 = new restaurant.MenuItems("Baby back ribs",11.35);
+        MenuItems item2 = new restaurant.MenuItems("coleslaw",5.35);
 
         System.out.println(item1.getName());
+        System.out.println(item2.getName());
     }
 }
 
